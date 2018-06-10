49dfc901cd33e68df9284fcbc3a82e03bd64a37e
Joacoferna <jafernandez1@miuandes.cl>
2018-06-10 19:53:24 -0400

    First commit. Login and registration views, calendar, and bottom navigation bar.

---
+++ app/src/main/java/com/example/joaco/workoutplan/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.joaco.workoutplan;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bot_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                return false;
+            }
+        });
+        if (savedInstanceState == null){
+            getSupportFragmentManager().
+            beginTransaction().replace(R.id.content, new CalendarFragment()).commit();
+        }
+    }
+
+}
